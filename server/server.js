13121fa57dad0a31937511dc79cef65edad31165
Amarin Sam <amarinsam@gmail.com>
2024-03-22 10:05:17 -0700

    Complete ETL code

--- server/server.js
+++ server/server.js
@@ -102,7 +102,7 @@ app.post('/photos', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening at port: ${PORT}`);
 });
